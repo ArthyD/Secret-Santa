ec5768ae628bfb078547c5cf2d1f22e58ae22d55
ArthurD <arthur.docquois@wanadoo.fr>
2024-01-01 19:05:02 +0100

    Final

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, flash, request
 from flask_login import login_required, current_user
 from .models import Flag
 import os
+from . import db
 from werkzeug.security import check_password_hash
 
 views = Blueprint('views', __name__)
@@ -16,18 +17,21 @@ def home():
         found = False
         for flag in flag_found:
             if check_password_hash(flag.hash, testflag):
-                found = True 
-                break
-
-            
+                found = True
+                flag.found = True
+                db.session.commit()
+                break   
         if(found):
             flash("Bien joué c'est un flag", category='success')
         else:
             flash("Raté cherches encore", category='error')
 
-
-    
-    return render_template("home.html", user=current_user, flags = flag_found)
+    all_ok = True
+    for flag in flag_found:
+        if(not flag.found):
+           all_ok=False
+           break
+    return render_template("home.html", user=current_user, flags = flag_found, all = all_ok)
 
 @views.route('/admin', methods=['GET'])
 @login_required
